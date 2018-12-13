c50a5a432376ac1fb0fc9f5f9b81885db2ad30f9
Rigo Carrasco <racarrasco@berkeley.edu>
2018-12-12 22:30:42 -0700

    second try

--- PythonPrograms/readWoollamVase.py
+++ PythonPrograms/readWoollamVase.py
@@ -24,6 +24,7 @@ class dataReader:
         self.ellipsometerType = int(self.info[28])
         self.rows = 0
         self.cols = 0
+        self.angles = []
         if(self.ellipsometerType == 3):
             self.rows = 4
             self.cols = 3
@@ -32,32 +33,56 @@ class dataReader:
             self.cols = 4
             self.file.readline()
         
-        theElements = []
-        energy = []
-        data1 = [] #the mueller matrix element
-        data2 = [] #repeated mueller matrix element
-        error1 = [] #error in mueller matrix element
-        error2 = []
+        
+        theElements = [[]]
+        energy = [[]]
+        data1 = [[]] #the mueller matrix element
+        data2 = [[]] #repeated mueller matrix element
+        error1 = [[]] #error in mueller matrix element
+        error2 = [[]]
         """Begin reading the data"""
+        k = 0
         for line in self.file.readlines():
+            
             data = line.strip().split("\t")
-            theElements.append(data[0].strip()) #label of the row
-            energy.append(float(data[1])) #energy 
-            data1.append(float(data[3])) #mueller matrix element or psi
+           
+        
+            if k == 0:
+                self.angles.append(float(data[2]))
+                k = k + 1
+            else:
+                if self.angles[-1] != float(data[2]):
+                    self.angles.append(float(data[2]))
+                    energy.append([])
+                    data1.append([])
+                    data2.append([])
+                    error1.append([])
+                    error2.append([])
+                    theElements.append([])
+            
+            theElements[-1].append(data[0].strip()) #label of the row
+            energy[-1].append(float(data[1])) #energy 
+            
+            data1[-1].append(float(data[3])) #mueller matrix element or psi
             
             
             if(data[0].strip()!= 'qJ' ): 
-                data2.append(float(data[4])) # repeated mueller matrix element or delta
-                error1.append(float(data[5])) # error in mueller matrix element
-                error2.append(float(data[6]))# error in other result
+                data2[-1].append(float(data[4])) # repeated mueller matrix element or delta
+                error1[-1].append(float(data[5])) # error in mueller matrix element
+                error2[-1].append(float(data[6]))# error in other result
                 
             else: #Jones Matrix quality factor
-                error1.append(float(data[4]))
+                error1[-1].append(float(data[4]))
                 
-        if self.unit == "1/cm\n": #Convert 1/cm to eV
-            energy = np.array(energy)
-            energy = energy/ 8065.6
+       
+#        print(len(energy))
+        energy = np.array(energy)
+        for i in range(len(energy)):
+            energy[i] = np.array(energy[i])
             
+        if self.unit == "1/cm\n": #Convert 1/cm to eV
+            energy = energy/ 8065.6
+        self.file.close()
         """Begin interpreting the data and creating the mueller matrix data structure""" 
         """A dictionary that contains each element and data type (ie: M12, AnE, qJ...)"""
         """IRVASE elements: MM12, MM13, MM21, MM22, MM23, MM31, MM32, MM33, MM41,MM42, MM43 
@@ -77,84 +102,93 @@ class dataReader:
         
         self.otherElements = ["AnE","Asp","Aps"]
 
-        self.MMenergy = {}
-        self.MMResult1 = {}
-        self.MMResult2 = {}     
-        self.MMerror1 = {}
-        self.MMerror2 = {}
-        self.MMtitle = {}
+        self.MMenergy = []
+        self.MMResult1 = []
+        self.MMResult2 = []     
+        self.MMerror1 = []
+        self.MMerror2 = []
+        self.MMtitle = []
         
         """Takes the the mueller matrix elements"""
-        
-        for index, element in enumerate(self.mElementNames):
-        #    print(element == theElements[0])
-            thaEnergy = [energy[index] for index, value 
-                         in enumerate(theElements) if value == element] 
-            """The data point to the corresponding energy value"""
-            thaelementResult1 = [data1[index] for index, value 
-                                 in enumerate(theElements) if value == element]
+        for i in range(len(self.angles)):
+            self.MMenergy.append({})
+            self.MMResult1.append({})
+            self.MMResult2.append({})    
+            self.MMerror1.append({})
+            self.MMerror2.append({})
+            self.MMtitle.append({})
+#            print("here is element1")
             
-            """Add the array to the dictionary for the element, so 
-            MMenergy["mm12"] will return an array of energies corresponding to 
-            the mueller matrix element "M12" """
-            self.MMenergy[element] = thaEnergy
-            self.MMResult1[element] = thaelementResult1
-           
-            self.MMtitle[element] = self.MplotTitles[index]
-            thaelementResult2 = [data2[index] for index, value 
-                                 in enumerate(theElements) if value == element]
-            self.MMResult2[element] = thaelementResult2
+            for index, element in enumerate(self.mElementNames):
+            #    print(element == theElements[0])
                 
-            thaerror1 = [error1[index] for index, value 
-                         in enumerate(theElements) if value == element]
-            self.MMerror1[element] = thaerror1
+                thaEnergy = [energy[i][index] for index, value 
+                             in enumerate(theElements[i]) if value == element] 
+                """The data point to the corresponding energy value"""
+                thaelementResult1 = [data1[i][index] for index, value 
+                                     in enumerate(theElements[i]) if value == element]
                 
-            thaerror2 = [error2[index] for index, value 
-                         in enumerate(theElements) if value == element]
-            self.MMerror2[element] = thaerror2
+                """Add the array to the dictionary for the element, so 
+                MMenergy["mm12"] will return an array of energies corresponding to 
+                the mueller matrix element "M12" """
+                self.MMenergy[i][element] = thaEnergy
+                self.MMResult1[i][element] = thaelementResult1
+               
+                self.MMtitle[i][element] = self.MplotTitles[index]
+                thaelementResult2 = [data2[i][index] for index, value 
+                                     in enumerate(theElements[i]) if value == element]
+                self.MMResult2[i][element] = thaelementResult2
+                    
+                thaerror1 = [error1[i][index] for index, value 
+                             in enumerate(theElements[i]) if value == element]
+                self.MMerror1[i][element] = thaerror1
+                    
+                thaerror2 = [error2[i][index] for index, value 
+                             in enumerate(theElements[i]) if value == element]
+                self.MMerror2[i][element] = thaerror2
+                
+                """Takes the relfectance ratios"""
+            for index, element in enumerate(self.otherElements):
+                thaEnergy = [energy[i][index] for index, value 
+                             in enumerate(theElements[i]) if value == element] 
             
-            """Takes the relfectance ratios"""
-        for index, element in enumerate(self.otherElements):
-            thaEnergy = [energy[index] for index, value 
-                         in enumerate(theElements) if value == element] 
-
-            thaEnergy = [energy[index] for index, value 
-                         in enumerate(theElements) if value == element] 
-            thaelementResult1 = [data1[index] for index, value 
-                                 in enumerate(theElements) if value == element]
+#                thaEnergy = [energy[i][index] for index, value 
+#                             in enumerate(theElements[i]) if value == element] 
+                thaelementResult1 = [data1[i][index] for index, value 
+                                     in enumerate(theElements[i]) if value == element]
+                
+                self.MMenergy[i][element] = thaEnergy
+                self.MMResult1[i][element] = thaelementResult1
+               
+               
+                thaelementResult2 = [data2[i][index] for index, value 
+                                     in enumerate(theElements[i]) if value == element]
+                self.MMResult2[i][element] = thaelementResult2
+                    
+                thaerror1 = [error1[i][index] for index, value 
+                             in enumerate(theElements[i]) if value == element]
+                self.MMerror1[i][element] = thaerror1
+                    
+                thaerror2 = [error2[i][index] for index, value 
+                             in enumerate(theElements[i]) if value == element]
+                self.MMerror2[i][element] = thaerror2
+            """Read the jones quality factor which is formatted differently"""
+            jonesEnergy = [energy[i][index] for index, value in enumerate(theElements[i]) 
+            if value == "qJ"]
             
-            self.MMenergy[element] = thaEnergy
-            self.MMResult1[element] = thaelementResult1
-           
-           
-            thaelementResult2 = [data2[index] for index, value 
-                                 in enumerate(theElements) if value == element]
-            self.MMResult2[element] = thaelementResult2
-                
-            thaerror1 = [error1[index] for index, value 
-                         in enumerate(theElements) if value == element]
-            self.MMerror1[element] = thaerror1
-                
-            thaerror2 = [error2[index] for index, value 
-                         in enumerate(theElements) if value == element]
-            self.MMerror2[element] = thaerror2
-        """Read the jones quality factor which is formatted differently"""
-        jonesEnergy = [energy[index] for index, value in enumerate(theElements) 
-        if value == "qJ"]
-        
-        jonesValue = [data1[index] for index, value in enumerate(theElements) 
-        if value == "qJ"]   
-        
-        jonesErr = [error1[index] for index, value in enumerate(theElements) 
-        if value == "qJ"]
-        self.MMenergy["qJ"] = jonesEnergy
-        self.MMResult1["qJ"] = jonesValue
-        self.MMerror1["qJ"] = jonesErr
+            jonesValue = [data1[i][index] for index, value in enumerate(theElements[i]) 
+            if value == "qJ"]   
+            
+            jonesErr = [error1[i][index] for index, value in enumerate(theElements[i]) 
+            if value == "qJ"]
+            self.MMenergy[i]["qJ"] = jonesEnergy
+            self.MMResult1[i]["qJ"] = jonesValue
+            self.MMerror1[i]["qJ"] = jonesErr
 
 
 
 class plotter():
-    def __init__(self, muellerData: dataReader, color:str, legend: str):
+    def __init__(self, muellerData: dataReader, legend: str):
         """Takes in a mueller matrix data set and plots the elements along 
         with the error bars"""
         rows = muellerData.rows
@@ -191,19 +225,19 @@ class plotter():
         jonesValue = muellerData.MMResult1["qJ"]
         
         
-        self.psiDelax.plot(psiDelEnergy, psi, color + '-', 
+        self.psiDelax.plot(psiDelEnergy, psi, '-', 
                            label = legend, linewidth = 1.0)
         self.psiDelax.set_ylabel('$\psi$')
         self.psiDelax.set_xlabel('Photon Energy (eV)')
         self.psiDelax.tick_params(which = "both", direction = "in")
         self.psiDelax.legend(frameon = False)
         
-        self.psiDelax2.plot(psiDelEnergy, delta, color+ '--', linewidth = 1.0)
+        self.psiDelax2.plot(psiDelEnergy, delta,'--', linewidth = 1.0)
         self.psiDelax2.set_ylabel('$\Delta$')
         self.psiDelax2.tick_params(which = "both", direction = "in")
         
         
-        self.jonesax.plot(jonesEnergy, jonesValue, color +'-', 
+        self.jonesax.plot(jonesEnergy, jonesValue,'-', 
                           label = legend, linewidth = 1.0)
         self.jonesax.set_xlabel('Photon Energy (eV)')
         self.jonesax.set_ylabel('Jones Quality Factor')
@@ -216,32 +250,32 @@ class plotter():
 #            muellerData.MMResult1[element], yerr = muellerData.MMerror1[element])
 #            print(index, element)
             self.axmuellerarray[index].plot(muellerData.MMenergy[element], 
-                               muellerData.MMResult1[element], color + '-', 
+                               muellerData.MMResult1[element], '-', 
                                label = legend, linewidth = 1.0)
             self.axmuellerarray[index].set_title(muellerData.MMtitle[element])
             self.axmuellerarray[index].tick_params(which = "both", direction ="in")
         handles, labels = self.axmuellerarray[0].get_legend_handles_labels()
         self.legendplot.legend(handles, labels, frameon = False, loc = 'center') 
                
-    def addPlot(self, moreData: dataReader, color: str, legend: str):
+    def addPlot(self, moreData: dataReader, legend: str):
         """allows user to plot multiple data sets in the same figure"""
         for index, element in enumerate(moreData.mElementNames):
 #            self.axmuellerarray[index].errorbar(moreData.MMenergy[element], moreData.MMResult1[element], yerr = moreData.MMerror1[element])
             self.axmuellerarray[index].plot(moreData.MMenergy[element],
                                moreData.MMResult1[element],
-                               color + '-', label = legend, linewidth = 1.0)
+                                '-', label = legend, linewidth = 1.0)
             
         self.psiDelax.plot(moreData.MMenergy["AnE"], 
                            moreData.MMResult1["AnE"], 
-                           color + '-', label = legend, linewidth = 1.0)
+                            '-', label = legend, linewidth = 1.0)
         
         self.psiDelax2.plot(moreData.MMenergy["AnE"], 
                             moreData.MMResult2["AnE"], 
-                            color + '--', linewidth = 1.0)
+                             '--', linewidth = 1.0)
         
         self.jonesax.plot(moreData.MMenergy["qJ"], 
                           moreData.MMResult1["qJ"], 
-                          color + '-', label = legend, linewidth = 1.0)
+                              '-', label = legend, linewidth = 1.0)
         
         self.psiDelax.legend(frameon = False)
         self.jonesax.legend(frameon = False)
